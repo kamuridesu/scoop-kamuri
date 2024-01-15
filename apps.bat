c2351205aa2551da4717e7cbb4e5a5ae5f4e8c0d
Kamuri Amorim <27641366+kamuridesu@users.noreply.github.com>
2024-01-15 09:15:11 -0300

    Create apps.bat

---
+++ apps.bat
@@ -0,0 +1,12 @@
+@echo off
+echo Initial setup...
+scoop install git
+scoop add bucket extras
+echo Installing packages
+scoop install python
+scoop install firefox
+scoop install qbittorrent
+scoop install vscode
+scoop install anydesk
+scoop install microsoft-teams
+scoop install scoop-search
