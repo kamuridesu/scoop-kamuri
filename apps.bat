f1562744c8714bbaa2b98a444323a9869881fcab
Kamuri Amorim <27641366+kamuridesu@users.noreply.github.com>
2024-01-15 17:45:01 -0300

    Update apps.bat

--- apps.bat
+++ apps.bat
@@ -3,16 +3,23 @@ echo Initial setup...
 powershell -command "Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser"
 powershell -command "Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression"
 scoop install git
-scoop add bucket extras
-echo Installing packages
-scoop install python
+scoop bucket add extras
+scoop bucket add java
+echo Installing basic packages
 scoop install firefox
 scoop install qbittorrent
-scoop install vscode
 scoop install anydesk
 scoop install microsoft-teams
 scoop install scoop-search
 scoop install nomacs
 scoop install mpc-hc-fork
 scoop install telegram
+echo Installing Development tools
+scoop install go
+scoop install maven
+scoop install gcc
+scoop install python
+scoop install vscode
+scoop install openjdk17
+scoop install nekoray
 echo Done!
